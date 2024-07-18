064a399cd5c5d99415d36c63b85857b6fc04c56c
dawidtt <dawid.gorszka@gmail.com>
2024-07-19 01:57:51 +0200

    Finish displayNumber and create displayOperator functions
    Now when to user will type any number or operator it will be displayed on the screen

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ function operate(operator, firstNumber, secondNumber) {
       console.log("you have to give operator");
   }
 }
-let operator = false;
+let isOperatorChosen = false;
+let operator = "";
 let tempNumber = "";
 let firstNumber = "";
 let secondNumber = "";
@@ -36,13 +37,25 @@ let secondNumber = "";
 function displayNumbers(number) {
   const resultPath = document.querySelector("#result p");
   tempNumber = number;
-  if (!operator) {
+  if (!isOperatorChosen) {
     firstNumber = firstNumber.concat(tempNumber);
     if (resultPath.textContent === "0") resultPath.textContent = "";
     resultPath.textContent += tempNumber;
-  } else secondNumber = secondNumber.concat(tempNumber);
+  } else {
+    secondNumber = secondNumber.concat(tempNumber);
+    resultPath.textContent += tempNumber;
+  }
   console.log(firstNumber);
   console.log(secondNumber);
+}
+
+function displayOperator(newOperator) {
+  const resultPath = document.querySelector("#result p");
+  if (firstNumber != "" && !isOperatorChosen) {
+    operator = newOperator;
+    resultPath.textContent += operator;
+    isOperatorChosen = true;
+  }
 }
 
 const numbersNodeList = document.querySelectorAll(".number");
@@ -51,4 +64,14 @@ const numbersArray = Array.from(numbersNodeList);
 console.log(numbersArray);
 for (const number of numbersArray) {
   number.addEventListener("click", () => displayNumbers(number.textContent));
+}
+
+const operatorsPathsNodeList = document.querySelectorAll(".operator");
+console.log(operatorsPathsNodeList);
+const operatorsPathsArray = Array.from(operatorsPathsNodeList);
+console.log(operatorsPathsArray);
+for (const operator of operatorsPathsArray) {
+  operator.addEventListener("click", () =>
+    displayOperator(operator.textContent)
+  );
 }
