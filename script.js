e6eee8d02954198921e75cd6e15d4e6f5fee4da1
dawidtt <dawid.gorszka@gmail.com>
2024-07-18 21:35:12 +0200

    Create operate function and add classes to buttons
    Function operate is needed to choose which operataion the user what to use.
    Classes are needed to get values from buttons such as numbers and operations.

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(firstNumber, secondNumber) {
 function divide(firstNumber, secondNumber) {
   return firstNumber / secondNumber;
 }
+
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+    default:
+      console.log("you have to give operator");
+  }
+}
+
+let operator = "";
+let firstNumber = 0;
+let secondNumber = 0;
